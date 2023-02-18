3eca50b1a05494a15f5535f6dd5bac12a9f42620
r-jeng <robinjeng0@gmail.com>
2023-02-17 18:24:29 -0800

    Set-up initial application

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import reservationsReducer from "../features/reservationSlice";
 
 export const store = configureStore({
   reducer: {
     // slices to be added here
-  }
-})
+    reservations: reservationsReducer,
+  },
+});
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
